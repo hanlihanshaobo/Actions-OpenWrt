f4d3bb71b73398978abfb1c0b53ae70c654c8133
hanlihanshaobo <50618558+hanlihanshaobo@users.noreply.github.com>
2021-01-16 22:01:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,11 +13,12 @@
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-#翻译及部分功能优化
-svn co https://github.com/QiuSimons/R2S-OpenWrt/trunk/PATCH/duplicate/addition-trans-zh-master /package/lean/lean-translate
-
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/new/luci-app-jd-dailybonus
 git clone -b master --single-branch https://github.com/tty228/luci-app-serverchan package/new/luci-app-serverchan
 git clone -b master --single-branch https://github.com/NateLol/luci-app-oled package/new/luci-app-oled
+
+#替换argon主题
+git clone -b master --single-branch https://github.com/jerrykuku/luci-theme-argon package/new/luci-theme-argon
+rm -rf package/lean/luci-theme-argon

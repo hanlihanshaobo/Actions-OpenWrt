b02974b4a72cb6a50a18b0354c0a2194b1fe53af
hanlihanshaobo <50618558+hanlihanshaobo@users.noreply.github.com>
2021-07-18 20:27:59 +0800

    Add files via upload

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,10 +11,12 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
 
 #添加server酱
 git clone -b master --single-branch https://github.com/tty228/luci-app-serverchan package/new/luci-app-serverchan
